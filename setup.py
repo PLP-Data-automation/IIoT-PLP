76eaab0fefa0b318eabb3bb1a415b883dc181202
Fuentes Juvera, Luis <luis.fuju@outlook.com>
2022-05-18 23:15:06 -0500

    IIoT-PLP python package. Dependencies:   DataAccess   DataBase

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name    = "IIoT-PLP",
+    version = "0.0.1",
+    author  = "Fuentes Juvera, Luis",
+    author_email    = "luis.fuju@outlook.com",
+    description     = "Python modulefor IIoT integration with Power BI",
+    long_description= long_description,
+    long_description_content_type = "text/markdown",
+    url     = "https://github.com/PLP-Data-automation/IIoT-PLP/",
+    project_urls={
+        "Bug Tracker": "https://github.com/PLP-Data-automation/IIoT-PLP/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir     = {"": "src"},
+    packages        = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8",
+    install_requires= [ 
+        "requests",
+        "pycryptodome",
+        "PyQt5",
+        "pandas",
+        "matplotlib"
+    ]
+)
